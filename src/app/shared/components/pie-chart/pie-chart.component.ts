b2269df3a634fa7ce5c2831d9c77efce4d9737a9
aatika <aatikat899@gmail.com>
2025-04-09 12:03:21 +0500

    graph_table

--- src/app/shared/components/pie-chart/pie-chart.component.ts
+++ src/app/shared/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { Chart } from 'chart.js';
 
 @Component({
@@ -7,41 +7,42 @@ import { Chart } from 'chart.js';
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss'
 })
-export class PieChartComponent {
+export class PieChartComponent implements AfterViewInit{
   @Input() pie_heading: string = '';
   @Input() pie_Data: any[] = [];
   @Input() pie_Labels: string[] = [];
   @Input() colors: string[] = [];
   @Input() showLabels: boolean = true;
 
-  chart = new Chart('defectPieChart', {
-    type: 'pie',
-    data: {
-      labels: ['a','b','c'],
-      datasets: [{
-        data: [10,15,70],
-        backgroundColor: [
-          '#4CAF50',
-          '#2196F3',
-          '#F44336'],
-        hoverOffset: 4
-      }]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        legend: {
-          position: 'top',
-          labels: {
-            boxWidth: 12
+  ngAfterViewInit() {
+    new Chart('defectPieChart', {
+      type: 'pie',
+      data: {
+        labels: ['a','b','c'],
+        datasets: [{
+          data: [10,15,70],
+          backgroundColor: [
+            '#4CAF50',
+            '#2196F3',
+            '#F44336'],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'top',
+            labels: {
+              boxWidth: 12
+            }
+          },
+          title: {
+            display: false
           }
-        },
-        title: {
-          display: false
         }
       }
-    }
-  });
+    });
+  }
 }
-
