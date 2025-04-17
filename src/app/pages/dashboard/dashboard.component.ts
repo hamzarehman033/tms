6c8af46fa0d1376abb14cdb41b496dc365722958
Usman <usman.asif@funavry.com>
2025-04-17 11:21:08 +0500

    ui

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LineGraphComponent } from '../../shared/components/line-graph/line-graph.component';
 import { PieChartComponent } from '../../shared/components/pie-chart/pie-chart.component';
 import { TableComponent } from '../../shared/components/table/table.component';
+import { IconComponent } from '../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LineGraphComponent, PieChartComponent, TableComponent],
+  imports: [LineGraphComponent, PieChartComponent, TableComponent, IconComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
